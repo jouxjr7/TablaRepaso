d05af03f22d3d33d6955c0eb3eb0105646f84981
jouxjr7 <joureyescr7@gmail.com>
2022-07-05 00:48:28 -0500

    qqq

---
+++ dojo.js
@@ -0,0 +1,11 @@
+function cambiar(){
+    if (document.getElementById("login").innerText=="logOut") {
+        document.getElementById("login").innerText="logIn"
+    }
+    else 
+        document.getElementById("login").innerText="logOut"
+    
+}
+function eliminar(){
+    document.getElementById("boton").innerHTML="";
+}
